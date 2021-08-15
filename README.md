05e8599025b102c108f2a08cae43f65a4d18be6e
Sourav R <developer.srv1@gmail.com>
2021-08-15 13:03:03 +0530

    🌱 birth of Emerald

---
+++ README.md
@@ -0,0 +1,7 @@
+# Laravel Blade Emerald
+
+> NOT YET READY
+
+## 👋🏼 Say Hi! 
+Leave a ⭐ if you find this package useful 👍🏼,
+don't forget to let me know in [Twitter](https://twitter.com/srvrksh)  
---
+++ src/EmeraldServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Aqua\Emerald;
+
+use Illuminate\Support\ServiceProvider;
+
+class EmeraldServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        
+    }
+}
