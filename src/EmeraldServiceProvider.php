11bb0d0d0483bd5da23bf51d3e0a17124b2b82eb
Sourav R <developer.srv1@gmail.com>
2021-08-15 15:34:29 +0530

    register emerald

--- src/EmeraldServiceProvider.php
+++ src/EmeraldServiceProvider.php
@@ -3,12 +3,20 @@
 namespace Aqua\Emerald;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
+use Aqua\Emerald\Components\Emerald;
 
 class EmeraldServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'emerald');
+
+        $this->loadViewComponentsAs('emerald', [
+            Emerald::class,
+        ]);
+
+        Blade::component('emerald', Emerald::class);
     }
 
     /**
