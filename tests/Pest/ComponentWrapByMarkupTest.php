5108a1838c0201699d1552d363c8a55aa32d7c32
Sourav R <developer.srv1@gmail.com>
2021-09-11 17:57:17 +0530

    trait usage tests

---
+++ tests/Pest/ComponentWrapByMarkupTest.php
@@ -0,0 +1,21 @@
+<?php
+
+test('the `Markup` trait can be used in blade component to wrap its content by generated markup using abbreviation', function () {
+    $view = (string) $this->blade('<x-emeraldtest-test-component wrap=".card > .card-body" />');
+    
+    expect($view)->toBe('<div class="card"><div class="card-body"><p>Lorem ipsum</p></div></div>');
+});
+
+test('when using the `Markup` trait emerald assumes the abbreviation receiver prop name to be `wrap`', function () {
+    expect(
+        (string) $this->blade('<x-emeraldtest-test-component wrap=".col" />')
+    )
+    ->toContain('class="col"');
+});
+
+test('when using the `Markup` trait the abbreviation receiver prop name can be customized', function () {
+    expect(
+        (string) $this->blade('<x-emeraldtest-test-component-custom-wrap-prop markup=".col" />')
+    )
+    ->toContain('class="col"');
+});
